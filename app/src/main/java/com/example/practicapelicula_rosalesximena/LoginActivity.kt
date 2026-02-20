f44d21db16e59d6d94e3aee715a849de60c09c30
xime-gif <rosalesximena2813@gmail.com>
2026-02-19 21:35:51 -0700

    Dark and Light theme

--- app/src/main/java/com/example/practicapelicula_rosalesximena/LoginActivity.kt
+++ app/src/main/java/com/example/practicapelicula_rosalesximena/LoginActivity.kt
@@ -1,6 +1,9 @@
 package com.example.practicapelicula_rosalesximena
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,36 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Inicializar variables de entrada
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
+
+        // Configurar evento al presionar el botón
+        btnEntrar.setOnClickListener {
+            // Obtener email y contraseña
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            // Validar que se ingresen email y contraseña
+            if (email.isEmpty()) {
+                etEmail.error = "Ingresa un correo"
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                etPassword.error = "Ingresa una contraseña"
+                return@setOnClickListener
+            }
+
+            // Abrir siguiente activity si se pasan las validaciones
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+            finish()
+        }
+
+
     }
 }
